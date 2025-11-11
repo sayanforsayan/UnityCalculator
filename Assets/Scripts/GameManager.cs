7cee24156abff73dbb17ab1bf9aa61637c61dd63
Sayan Jana <sayanforsayan@outlook.com>
2025-11-12 01:52:53 +0530

    Functional Ready

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,111 @@
+using System.Collections.Generic;
 using UnityEngine;
-
-namespace Sayan.UnityCalculators
+namespace Sayan.UnityCalculator
 {
     public class GameManager : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        public static GameManager Instance;
+
+        private void Awake()
         {
-        
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        public float Calculate(string expression)
         {
-        
+            List<string> storeDigit = SeparateNumbersAndOperators(expression);
+            return CalculateWithRule(storeDigit);
+        }
+
+        private List<string> SeparateNumbersAndOperators(string expression)
+        {
+            List<string> storeDigit = new List<string>();
+            string temp = "";
+
+            foreach (char c in expression)
+            {
+                if (char.IsDigit(c) || c == '.')
+                {
+                    temp += c;
+                }
+                else
+                {
+                    if (temp != "")
+                    {
+                        storeDigit.Add(temp);
+                        temp = "";
+                    }
+                    storeDigit.Add(c.ToString());
+                }
+            }
+
+            if (temp != "")
+            {
+                storeDigit.Add(temp);
+            }
+
+            return storeDigit;
+        }
+
+        private float CalculateWithRule(List<string> parts)
+        {
+            //Division and Multiplication
+            for (int i = 0; i < parts.Count; i++)
+            {
+                string symbol = parts[i];
+                Debug.Log(symbol);
+                if (symbol == "/" || symbol == "*")
+                {
+                    float left = float.Parse(parts[i - 1]);
+                    float right = float.Parse(parts[i + 1]);
+
+                    float result = 0;
+
+                    if (symbol == "/")
+                    {
+                        result = left / right;
+
+                    }
+                    else if (symbol == "*")
+                    {
+                        result = left * right;
+                    }
+
+                    parts[i - 1] = result.ToString();
+
+                    parts.RemoveAt(i);
+                    parts.RemoveAt(i);
+
+                    i--;
+                }
+            }
+
+            // Addition and Subtraction
+            float finalResult = float.Parse(parts[0]);
+
+            for (int i = 1; i < parts.Count; i += 2)
+            {
+                string symbol = parts[i];
+                float nextNumber = float.Parse(parts[i + 1]);
+
+                if (symbol == "+")
+                {
+                    finalResult += nextNumber;
+                }
+                else if (symbol == "-")
+                {
+                    finalResult -= nextNumber;
+                }
+            }
+
+            return finalResult;
         }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,19 +1,52 @@
 using UnityEngine;
+using TMPro;
 
-namespace Sayan.UnityCalculators
+namespace Sayan.UnityCalculator
 {
     public class UIManager : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        [SerializeField] private TMP_Text expressionText;
+        [SerializeField] private TMP_Text resultText;
+
+        private string expression = "";
+
         void Start()
         {
-        
+            OnClearAll();
         }
 
-        // Update is called once per frame
-        void Update()
+        public void OnButtonClick(string value)
         {
-        
+            if (!string.IsNullOrEmpty(resultText.text.ToString()))
+            {
+                expression = "";
+                resultText.text = "";
+            }
+
+            if (value == ".")
+            {
+                string[] parts = expression.Split('+', '-', '*', '/');
+                string currentPart = parts[parts.Length - 1];
+                if (currentPart.Contains(".")) return;
+            }
+
+            expression += value;
+            expressionText.text = expression;
+        }
+
+        public void OnEqualClick()
+        {
+            if (string.IsNullOrEmpty(expression)) return;
+            float result = GameManager.Instance.Calculate(expression);
+            expressionText.text = "";
+            resultText.text = result.ToString();
+        }
+
+        public void OnClearAll()
+        {
+            expression = "";
+            expressionText.text = "";
+            resultText.text = "";
         }
     }
 }
